3b33b0b9ddc332964900127ea5ee87127a9f5cad
zyjeng <zyjeng11@gmail.com>
2020-02-09 20:53:26 +0800

    add read map method

--- src/map/ReadMap.java
+++ src/map/ReadMap.java
@@ -1,8 +1,25 @@
 package map;
 
+import java.io.*;
+
 public class ReadMap implements MapConfig{
 	
 	private void read() {
+		
+		String path = getClass().getResource(relativePath).getPath();
+		try(FileInputStream fin = new FileInputStream(path);
+			DataInputStream din = new DataInputStream(fin);)
+		{
+			for(int i=0; i<map1.length; i++) {
+				for(int j=0; j<map1.length; j++) {
+					map1[i][j] = din.readInt();
+					map2[i][j] = din.readInt();
+				}
+			}
+			
+		}catch(IOException ex) {
+			ex.printStackTrace();
+		}
 		
 	}
 }
