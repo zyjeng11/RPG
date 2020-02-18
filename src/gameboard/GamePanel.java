1df4794372056c9c054a8b7b05397095795dac74
zyjeng <zyjeng11@gmail.com>
2020-02-18 15:03:19 +0800

    clean up

--- src/gameboard/GamePanel.java
+++ src/gameboard/GamePanel.java
@@ -32,14 +32,12 @@ public class GamePanel extends JPanel implements MapConfig {
 			for (int j = 0; j < smallIconsMap1.length; j++) {
 				if (smallIconsMap1[i][j] != null)
 					smallIconsMap1[i][j].paintIcon(null, g, i*eleWidth, j*eleHeight);
-					//g.drawImage(smallIconsMap1[i][j].getImage(), i * eleWidth, j * eleHeight, eleWidth, eleHeight, null);
 			}
 		}
 		for (int i = 0; i < smallIconsMap2[0].length; i++) {
 			for (int j = 0; j < smallIconsMap2.length; j++) {
 				if (smallIconsMap2[i][j] != null)
 					smallIconsMap2[i][j].paintIcon(null, g, i*eleWidth, j*eleHeight);
-					//g.drawImage(smallIconsMap2[i][j].getImage(), i * eleWidth, j * eleHeight, eleWidth, eleHeight, null);
 			}
 		}
 	}
